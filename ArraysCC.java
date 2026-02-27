89f7506d57b45e28dc56cbe0b02983aa6b19f06e
Pratik Salunke <pratiksalunke3112@gmail.com>
2026-02-27 13:33:31 +0530

    Reverse an array

--- ArraysCC.java
+++ ArraysCC.java
@@ -102,32 +102,58 @@
 
 
 //Binary search
-import java.util.*;
+// import java.util.*;
+// public class ArraysCC {
+//     public static int binarySearch(int numbers[], int key) {
+//         int start = 0 ,  end = numbers.length-1;
+
+//         while(start <= end) {
+//             int mid = (start + end) / 2;
+
+//             //comparison
+//             if(numbers[mid] == key ) { // found
+//                 return mid;
+//             }
+//             if(numbers[mid] < key ) { // right
+//                 start = mid+1;
+//             }else {//left
+//                 end = mid - 1;
+//             }   
+//         }
+
+//         return -1;
+//     }
+//     public static void main(String args[]){
+//         int numbers[] = {2,4,6,8,10,12,14,16 };
+//         int key = 10;
+
+//         System.out.println("Index for key is :" + binarySearch(numbers , key));
+
+//     }
+// }
+
+//Reverse an array
+ import java.util.*;
 public class ArraysCC {
-    public static int binarySearch(int numbers[], int key) {
-        int start = 0 ,  end = numbers.length-1;
+    public static void reverse(int numbers[]) {
+        int first= 0 ,  last = numbers.length-1;
 
-        while(start <= end) {
-            int mid = (start + end) / 2;
+        while(first < last) {
+            int temp = numbers[last];
+            numbers[last] = numbers[first];
+            numbers[first] = temp;
 
-            //comparison
-            if(numbers[mid] == key ) { // found
-                return mid;
-            }
-            if(numbers[mid] < key ) { // right
-                start = mid+1;
-            }else {//left
-                end = mid - 1;
-            }   
+            first++;
+            last--;
         }
 
-        return -1;
     }
     public static void main(String args[]){
         int numbers[] = {2,4,6,8,10,12,14,16 };
-        int key = 10;
-
-        System.out.println("Index for key is :" + binarySearch(numbers , key));
-
+        reverse(numbers);
+        for(int i = 0; i<numbers.length; i++) {
+            System.out.print(numbers[i])
+        }
+        System.out.println();
     }
 }
