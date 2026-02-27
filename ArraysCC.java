8d65158fa9f62a8769a7b3851bc6fd6814845c38
Pratik Salunke <pratiksalunke3112@gmail.com>
2026-02-27 13:49:24 +0530

    Pairs in array

--- ArraysCC.java
+++ ArraysCC.java
@@ -133,27 +133,50 @@
 // }
 
 //Reverse an array
- import java.util.*;
-public class ArraysCC {
-    public static void reverse(int numbers[]) {
-        int first= 0 ,  last = numbers.length-1;
+//  import java.util.*;
+// public class ArraysCC {
+//     public static void reverse(int numbers[]) {
+//         int first= 0 ,  last = numbers.length-1;
 
-        while(first < last) {
-            int temp = numbers[last];
-            numbers[last] = numbers[first];
-            numbers[first] = temp;
+//         while(first < last) {
+//             int temp = numbers[last];
+//             numbers[last] = numbers[first];
+//             numbers[first] = temp;
 
-            first++;
-            last--;
-        }
+//             first++;
+//             last--;
+//         }
 
-    }
-    public static void main(String args[]){
-        int numbers[] = {2,4,6,8,10,12,14,16 };
-        reverse(numbers);
-        for(int i = 0; i<numbers.length; i++) {
-            System.out.print(numbers[i])
-        }
-        System.out.println();
-    }
-}
+//     }
+//     public static void main(String args[]){
+//         int numbers[] = {2,4,6,8,10,12,14,16 };
+//         reverse(numbers);
+//         for(int i = 0; i<numbers.length; i++) {
+//             System.out.print(numbers[i] + " ");
+//         }
+//         System.out.println();
+//     }
+// }
+
+
+//Pairs in array
+// import java.util.*;
+// public class ArraysCC {
+//     public static void printPairs(int numbers[]) {
+//         int tp =0;
+//        for(int i=0; i<numbers.length; i++) {
+//         int curr = numbers[i]; //2,4,6,8,10
+//         for(int j=i+1; j<numbers.length; j++) {
+//             System.out.print("(" + curr + "," + numbers[j] + ") ");
+//             tp++;
+//         }
+//         System.out.println();
+//        }
+//         System.out.println("total pairs : " + tp);
+
+//     }
+//     public static void main(String args[]){
+//         int numbers[] = {2,4,6,8,10,12,14,16 };
+//         printPairs(numbers);
+//     }
+// }
