8eb3af8dd1848d77e5d383af7a17f754928161d6
Pratik Salunke <pratiksalunke3112@gmail.com>
2026-02-27 08:47:19 +0530

    Passing arrays as argument

--- ArraysCC.java
+++ ArraysCC.java
@@ -1,26 +1,47 @@
-import java.util.*;
+// To printing marks using an array
+// import java.util.*;
 
-public class ArraysCC {
-    public static void main(String args[]) {
-        int marks[] = new int[100];
+// public class ArraysCC {
+//     public static void main(String args[]) {
+//         int marks[] = new int[100];
 
-        Scanner sc = new Scanner(System.in);
+//         Scanner sc = new Scanner(System.in);
 
-         System.out.println("length:" + marks.length);
+//          System.out.println("length:" + marks.length);
 
-        marks[0] = sc.nextInt();
-        marks[1] = sc.nextInt();
-        marks[2] = sc.nextInt();
+//         marks[0] = sc.nextInt();
+//         marks[1] = sc.nextInt();
+//         marks[2] = sc.nextInt();
         
-        System.out.println("phy:" + marks[0] );
-        System.out.println("chem:" + marks[1] );
-        System.out.println("math:" + marks[2] );
+//         System.out.println("phy:" + marks[0] );
+//         System.out.println("chem:" + marks[1] );
+//         System.out.println("math:" + marks[2] );
 
-        // marks[2] = 100;
-        // marks[2] = marks[2] +1;
-        // System.out.println("math:" + marks[2] );
+//         // marks[2] = 100;
+//         // marks[2] = marks[2] +1;
+//         // System.out.println("math:" + marks[2] );
 
-        int percentage = (marks[0] + marks[1] + marks[2]) / 3;
-        System.out.println("percentage=" + percentage+ "%" );
+//         int percentage = (marks[0] + marks[1] + marks[2]) / 3;
+//         System.out.println("percentage=" + percentage+ "%" );
+//     }
+// }
+
+
+//Passing arrays as argument
+import java.util.*;
+public class ArraysCC {
+    public static void update(int marks[]) {
+        for(int i=0; i<marks.length; i++) {
+            marks[i] = marks[i] +1 ;
+        }
+    }
+    public static void main(String args[]){
+        int marks[] = {97, 98 ,99 };
+        update(marks);
+
+        for(int i = 0; i<marks.length ; i++) {
+            System.out.print(marks[i] + " ");
+        }
+          System.out.println();
     }
 }
