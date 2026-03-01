dfb6e142cc5fcf791338bd2a3b225fe35d5d6665
Pratik Salunke <pratiksalunke3112@gmail.com>
2026-03-01 12:13:08 +0530

    //Buy and Sell Stocks

--- ArraysCC.java
+++ ArraysCC.java
@@ -305,39 +305,65 @@
 
 
 // Trapping of Rain Water 22 no. problem in DSA sheet
-import java.util.*;
+// import java.util.*;
+//O(n) will be TC for this code
+// public class ArraysCC{
+//     public static int trappedRainWater(int height[]) {
+//         int n = height.length;
+//         //to calculate left max boundry -> array
+//         int leftMax[] = new int[n];
+//         leftMax[0] = height[0];
+//         for(int i=1; i<n; i++) {
+//             leftMax[i] = Math.max(height[i] , leftMax[i-1]);
+//         }
 
-public class ArraysCC{
-    public static int trappedRainWater(int height[]) {
-        int n = height.length;
-        //to calculate left max boundry -> array
-        int leftMax[] = new int[n];
-        leftMax[0] = height[0];
-        for(int i=1; i<n; i++) {
-            leftMax[i] = Math.max(height[i] , leftMax[i-1]);
-        }
+//         //to calculate right max boundry -> array
+//         int rightMax[] = new int[n];
+//         rightMax[n-1] = height[n-1];
+//         for(int i= n-2; i>=0; i--) {
+//             rightMax[i] = Math.max(height[i] , rightMax[i+1]);
+//         }
 
-        //to calculate right max boundry -> array
-        int rightMax[] = new int[n];
-        rightMax[n-1] = height[n-1];
-        for(int i= n-2; i>=0; i--) {
-            rightMax[i] = Math.max(height[i] , rightMax[i+1]);
-        }
-
-        int trappedWater = 0;
-        //loop
-        for(int i=0; i<n; i++) {
-            //waterLevel = min(leftmax bound , rightmax boundry)
-            int waterLevel = Math.min(leftMax[i] , rightMax[i]);
+//         int trappedWater = 0;
+//         //loop
+//         for(int i=0; i<n; i++) {
+//             //waterLevel = min(leftmax bound , rightmax boundry)
+//             int waterLevel = Math.min(leftMax[i] , rightMax[i]);
             
-            //trapped water = waterLevel - height[i]
-            trappedWater += waterLevel - height[i];
-        }
-        return trappedWater;
-    }
+//             //trapped water = waterLevel - height[i]
+//             trappedWater += waterLevel - height[i];
+//         }
+//         return trappedWater;
+//     }
 
-    public static void main(String args[]) {
-        int height[] = {4,2,0,6,3,2,5};
-        System.out.println( trappedRainWater(height));
-    }
-}
+//     public static void main(String args[]) {
+//         int height[] = {4,2,0,6,3,2,5};
+//         System.out.println( trappedRainWater(height));
+//     }
+// }
+
+
+//Buy and Sell Stocks
+// import java.util.*;
+
+// public class ArraysCC {
+//     public static int buyAndSellStocks(int prices[]) {
+//         int buyPrice = Integer.MAX_VALUE;
+//         int maxProfit = 0;
+
+//         for(int i=0; i<prices.length; i++) {
+//             if(buyPrice < prices[i]) { //profit
+//                 int profit = prices[i] - buyPrice; //todays profit
+//                 maxProfit = Math.max(maxProfit , profit);
+//             } else {
+//                 buyPrice = prices[i];
+//             }
+//         }
+//         return maxProfit;
+//     }
+
+//     public static void main(String args[]) {
+//         int prices[] = {7,1,5,3,6,4};
+//         System.out.println(buyAndSellStocks(prices));
+//     }
+// }
