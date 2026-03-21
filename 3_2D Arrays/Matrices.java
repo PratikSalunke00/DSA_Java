0ab3d3ed62a122d162b9321fec74e6bd434f66b3
Pratik Salunke <pratiksalunke3112@gmail.com>
2026-03-21 19:34:27 +0530

    // Diagonal Sum of a Matrix

--- 3_2D Arrays/Matrices.java
+++ 3_2D Arrays/Matrices.java
@@ -38,3 +38,103 @@
 //         search(matrix, 5);
 //     }
 // }
+
+
+// Spiral Matrix
+// import java.util.*;
+// public class Matrices {
+
+//     public static void printSpiral(int matrix[][]){
+//         int startRow = 0;
+//         int endRow = matrix.length - 1;
+//         int startCol = 0;
+//         int endCol = matrix[0].length - 1;
+
+//         while (startRow <= endRow && startCol <= endCol) {
+//             // top borders 
+//             for (int j = startCol; j <= endCol; j++) {
+//                 System.out.print(matrix[startRow][j] + " ");
+//             }
+
+//             // right borders
+//             for (int i = startRow + 1; i <= endRow; i++) {
+//                 System.out.print(matrix[i][endCol] + " ");
+//             }
+
+//             // bottom borders
+//             for (int j = endCol - 1; j >= startCol; j--) {
+//                 if (startRow == endRow) {
+//                     break;
+//                 }
+//                 System.out.print(matrix[endRow][j] + " ");
+//             }
+
+//             // left
+//             for (int i = endRow - 1; i > startRow; i--) {
+//                 if (startCol == endCol) {
+//                     break;
+//                 }
+//                 System.out.print(matrix[i][startCol] + " ");
+//             }
+
+//             startRow++;
+//             endRow--;
+//             startCol++;
+//             endCol--;
+//         }
+//         System.out.println();
+//     }
+
+//     public static void main(String[] args) {
+//         int matrix[][] = {{1,2,3,4},
+//                           {5,6,7,8},
+//                           {9,10,11,12},
+//                           {13,14,15,16}};
+//         printSpiral(matrix);    
+//     }
+// }
+
+
+// Diagonal Sum of a Matrix
+// import java.util.*;
+// public class Matrices {
+
+//     public static int diagonalSum(int matrix[][]) {
+//         int sum = 0;
+
+//         // with O(n^2) time complexity
+//         // for (int i = 0; i < matrix.length; i++) {
+//         //    for (int j = 0; j < matrix[0].length; j++) {
+//         //         if (i == j) {
+//         //             sum += matrix[i][j];
+//         //         }
+//         //         else if (i + j == matrix.length - 1) {
+//         //             sum += matrix[i][j];
+//         //         }
+//         //     }
+//         // }
+
+//         // with O(n) time complexity
+//         for (int i = 0; i < matrix.length; i++) {
+//             // primary diagonal
+//             sum += matrix[i][i]; 
+
+//             // secondary diagonal
+//             if (i != matrix.length - 1 - i) {
+//                 // secondary diagonal
+//                 sum += matrix[i][matrix.length - 1 - i];
+//             }
+
+//         }
+                    
+//         return sum;
+//     }
+
+//     public static void main(String[] args) {
+//         int matrix[][] = {{1,2,3,4},
+//                           {5,6,7,8},
+//                           {9,10,11,12},
+//                           {13,14,15,16}};
+//         System.out.println(diagonalSum(matrix));  
+//     }
+// }
