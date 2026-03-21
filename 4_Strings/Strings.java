6b1f3b933d8b8a822f92c413455b02c972775034
Pratik Salunke <pratiksalunke3112@gmail.com>
2026-03-21 22:25:23 +0530

    Starting to String

---
+++ 4_Strings/Strings.java
@@ -0,0 +1,17 @@
+//Strings in Java
+//Strings are a sequence of characters.
+// import java.util.*;
+// public class Strings {
+//     public static void main(String[] args) {
+//         char arr[] = {'h', 'e', 'l', 'l', 'o'};
+//         String str = "abcd";
+//         String str2 = new String("abcd");
+
+//         //Strings are immutable in java
+
+//         Scanner sc = new Scanner(System.in);
+//         String name ;
+//         name = sc.nextLine();
+//         System.out.println(name);
+//     }
+// }
