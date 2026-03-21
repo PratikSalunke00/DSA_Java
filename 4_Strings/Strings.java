7408cce5dc143875e79bcc3657ca40712320c4b5
Pratik Salunke <pratiksalunke3112@gmail.com>
2026-03-21 22:30:50 +0530

    //Functions of String class

--- 4_Strings/Strings.java
+++ 4_Strings/Strings.java
@@ -15,3 +15,21 @@
 //         System.out.println(name);
 //     }
 // }
+
+
+//Functions of String class
+// import java.util.*;
+// public class Strings {
+//     public static void main(String[] args) {
+//         String str = "Hello World";
+//         System.out.println(str.length()); //length of the string
+//         System.out.println(str.charAt(0)); //character at index 0
+//         System.out.println(str.indexOf('o')); //index of first occurrence of 'o'
+//         System.out.println(str.lastIndexOf('o')); //index of last occurrence of 'o'
+//         System.out.println(str.substring(0, 5)); //substring from index 0 to 5
+//         System.out.println(str.toUpperCase()); //convert to uppercase
+//         System.out.println(str.toLowerCase()); //convert to lowercase
+//         System.out.println(str.trim()); //remove leading and trailing spaces
+//         System.out.println(str.replace('o', 'a')); //replace 'o' with 'a'
+//     }
+// }
