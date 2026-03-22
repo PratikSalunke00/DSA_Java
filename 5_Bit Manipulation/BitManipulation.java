4ae11c07a0f11381d3f726521e82969d81fcda5e
Pratik Salunke <pratiksalunke3112@gmail.com>
2026-03-22 21:41:41 +0530

    // Bit manipulation in Java

---
+++ 5_Bit Manipulation/BitManipulation.java
@@ -0,0 +1,10 @@
+// Bit manipulation in Java
+// import java.util.*;
+// public class BitManipulation {
+//     public static void main(String args[]) {
+//         System.out.println(5 & 6); // bitwise and
+//         System.out.println(5 | 6); // bitwise or
+//         System.out.println(5 ^ 6); // bitwise xor
+//         System.out.println(~5); // bitwise not
+//     }
+// }
