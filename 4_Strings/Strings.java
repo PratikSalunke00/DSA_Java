f5c0faf96713517d2a71734f241c92af8156f3bb
Pratik Salunke <pratiksalunke3112@gmail.com>
2026-03-22 08:28:41 +0530

    //Palindrome check

--- 4_Strings/Strings.java
+++ 4_Strings/Strings.java
@@ -33,3 +33,43 @@
 //         System.out.println(str.replace('o', 'a')); //replace 'o' with 'a'
 //     }
 // }
+
+
+//Concatenation of strings
+// import java.util.*;
+// public class Strings {
+//     public static void main(String[] args) {
+//         String str1 = "Hello";
+//         String str2 = "World";
+//         //concatenation using + operator
+//         String str3 = str1 + " " + str2; 
+//         System.out.println(str3);
+
+//         //concatenation using concat() method
+//         String str4 = str1.concat(" ").concat(str2); 
+//         System.out.println(str4);
+//     }
+// }
+
+
+//Palindrome check
+// import java.util.*;
+// public class Strings {
+//     public static boolean isPalindrome(String str) {
+//         for(int i=0; i<str.length()/2; i++) {
+//             int n = str.length();
+//             if(str.charAt(i) != str.charAt(n-1-i)) {
+//                 //not a palindrome
+//                 return false;
+//             }
+//         }
+//         //palindrome
+//         return true; 
+//     }
+//     public static void main(String[] args) {
+//         String str = "racecar";
+//         String str2 = "hello";
+//         System.out.println(isPalindrome(str));
+//         System.out.println(isPalindrome(str2));
+//     }
+// }
