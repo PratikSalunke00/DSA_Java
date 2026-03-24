22f4ff5332834f164c8acbf3e02ea84afaeb637e
Pratik Salunke <pratiksalunke3112@gmail.com>
2026-03-24 15:17:17 +0530

    //static keyword

---
+++ 6_OOPs/StaticKeyword.java
@@ -0,0 +1,30 @@
+//static keyword
+// public class StaticKeyword {
+//     public static void main(String[] args) {
+//         Student s1 = new Student();
+//         s1.schoolName = "ABC School";
+
+//         Student s2 = new Student();
+//         System.out.println(s2.schoolName); // ABC School
+
+//         Student s3 = new Student();
+//         s3.schoolName = "XYZ School";
+
+//         System.out.println(s1.schoolName); // XYZ School
+//     }
+// }
+
+// class Student{
+//     String name;
+//     int roll;
+
+//     static String schoolName;
+
+//     void setName(String name){
+//         this.name = name;
+//     }
+
+//     String getName(){
+//         return this.name;
+//     }
+// }
