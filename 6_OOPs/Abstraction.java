4481d472b42796d6cec0b88ffc4d5a50bc15585d
Pratik Salunke <pratiksalunke3112@gmail.com>
2026-03-24 14:26:48 +0530

    Abstraction

---
+++ 6_OOPs/Abstraction.java
@@ -0,0 +1,32 @@
+//Abstraction
+// public class Abstraction {
+//     public static void main(String[] args) {
+//         Horse horse = new Horse();
+//         horse.eat(); // Inherited method
+//         horse.walk(); // Implemented abstract method
+
+//         Chicken chicken = new Chicken();
+//         chicken.eat(); // Inherited method
+//         chicken.walk(); // Implemented abstract method
+//     }
+// }
+
+// abstract class Animal{
+//     void eat(){
+//         System.out.println("Animal is eating");
+//     }
+
+//     abstract void walk();
+// }
+
+// class Horse extends Animal{
+//     void walk(){
+//         System.out.println("Horse is walking");
+//     }
+// }
+
+// class Chicken extends Animal{
+//     void walk(){
+//         System.out.println("Chicken is walking");
+//     }
+// }
---
+++ 6_OOPs/Packages.java
@@ -0,0 +1,22 @@
+//inbuilt package
+// import java.util.*;
+// public class Packages {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter the package name:");
+//         String packageName = sc.nextLine();
+//         System.out.println("You entered: " + packageName);
+//         // You can use the package name to import classes or perform other operations
+//         sc.close();
+//     }
+// }
+
+
+//user defined package
+// import mypackage.*;
+// public class Packages {
+//     public static void main(String[] args) {
+//         MyClass obj = new MyClass();
+//         obj.display();
+//     }
+// }
