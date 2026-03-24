7e3523d59b4e7fa067df7678770347616fc0c83c
Pratik Salunke <pratiksalunke3112@gmail.com>
2026-03-24 15:30:49 +0530

    super keyword

---
+++ 6_OOPs/SuperKeyword.java
@@ -0,0 +1,23 @@
+//Super keyword
+// public class SuperKeyword {
+//     public static void main(String[] args) {
+//         Horse h = new Horse();
+//         // System.out.println(h.color);
+//     }
+// }
+
+// class Animal{
+//     String color;
+//     Animal(){
+//         System.out.println("Animal Constructor");
+//     }
+// }
+
+// class Horse extends Animal{
+//     Horse(){
+//         super(); // Calls the constructor of the parent class (Animal)
+//         // super.color = "Brown";
+        
+//         System.out.println("Horse Constructor");
+//     }
+// }
