a334c4b80eb79c04789d223f06112b00a1e28131
Pratik Salunke <pratiksalunke3112@gmail.com>
2026-03-24 15:12:00 +0530

    // Interfaces in Java

---
+++ 6_OOPs/Interfaces.java
@@ -0,0 +1,29 @@
+// Interfaces in Java
+public class Interfaces {
+    public static void main(String[] args) {
+      Queen q = new Queen();
+        q.moves();
+    }
+}
+
+interface ChessPlayer{
+    void moves();
+}
+
+class Queen implements ChessPlayer{
+    public void moves(){
+        System.out.println("up, down, left, right, diagonal");
+    }
+}
+
+class Rook implements ChessPlayer{
+    public void moves(){
+        System.out.println("up, down, left, right");
+    }
+}
+
+class King implements ChessPlayer{
+    public void moves(){
+        System.out.println("one step in any direction");
+    }
+}
