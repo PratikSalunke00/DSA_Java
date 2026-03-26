506d27d04f7fda33793f011886fbca1bf0fba5ba
Pratik Salunke <pratiksalunke3112@gmail.com>
2026-03-26 20:39:38 +0530

    //To calculate the factorial of a number using recursion

--- 7_Recursion/RecursionBasics.java
+++ 7_Recursion/RecursionBasics.java
@@ -1,4 +1,5 @@
 //Recursion Basics
+//To print numbers from n to 1 using recursion
 // public class RecursionBasics {
 //     public static void printDec(int n) {
 //         if (n == 1) {
@@ -12,5 +13,43 @@
 //     public static void main(String[] args) {
 //         int n = 10;
 //         printDec(n);
+//     }
+// }
+
+
+//To print numbers from 1 to n using recursion
+// public class RecursionBasics {
+//     public static void printInc(int n) {
+//         if (n == 1) {
+//             System.out.println(n);
+//             return;
+//         }
+//         printInc(n - 1);
+//         System.out.println(n);
+//     }
+//     public static void main(String[] args) {
+//         int n = 10;
+//         printInc(n);
+//     }
+// }
+
+
+//To calculate the factorial of a number using recursion
+// public class RecursionBasics {
+//     public static int fact(int n){
+//         // Base case
+//         if(n == 0 ){
+//             return 1;
+//         }
+
+//         //fnm1 = factorial of n-1
+//         int fnm1 = fact(n-1);
+//         int fn = n * fnm1;
+//         return fn;
+//     }
+
+//     public static void main(String[] args) {
+//         int n = 5;
+//         System.out.println(fact(n));
 //     }
 // }
