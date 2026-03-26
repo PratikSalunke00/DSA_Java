d1b5cdcb48531397b56c4b406f54e5b2e78f9018
Pratik Salunke <pratiksalunke3112@gmail.com>
2026-03-26 19:52:58 +0530

    RecursionBasics

---
+++ 7_Recursion/RecursionBasics.java
@@ -0,0 +1,16 @@
+//Recursion Basics
+// public class RecursionBasics {
+//     public static void printDec(int n) {
+//         if (n == 1) {
+//             System.out.println(n);
+//             return;
+//         }
+//         System.out.println(n);
+//         // Recursive call with n-1 (it performs the looping)
+//         printDec(n - 1);
+//     }
+//     public static void main(String[] args) {
+//         int n = 10;
+//         printDec(n);
+//     }
+// }
