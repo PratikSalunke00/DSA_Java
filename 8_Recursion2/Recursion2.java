d2ae184ad5c901b93e8c77fcaa27c40776f57111
Pratik Salunke <pratiksalunke3112@gmail.com>
2026-03-28 12:58:40 +0530

    // Friends Pairing Problem

--- 8_Recursion2/Recursion2.java
+++ 8_Recursion2/Recursion2.java
@@ -52,3 +52,31 @@
 //         System.out.println(removeDuplicates(str, 0, new StringBuilder(""), map));
 //     }
 // }
+
+
+// Friends Pairing Problem
+// public class Recursion2 {
+//     public static int friendsPairing(int n){
+//         //base case
+//         if( n == 1 || n == 2){
+//             return n;
+//         }
+//        //choice
+//        //single
+//         int fnm1 = friendsPairing(n-1);
+
+//         //pair
+//         int fnm2 = friendsPairing(n-2);
+//         int pairedWays = (n-1) * fnm2;
+
+//         int totalWays = fnm1 + pairedWays;
+//         return totalWays;
+
+//         //or we can directly return without storing in variables
+//         //return friendsPairing(n-1) + (n-1) * friendsPairing(n-2);
+//     }
+
+//     public static void main(String[] args) {
+//         System.out.println(friendsPairing(3));
+//     }
+// }
