5b2eecf3b90e25746911e00a1587155ca1cf5d2c
Pratik Salunke <pratiksalunke3112@gmail.com>
2026-03-28 13:40:05 +0530

    // Print Binary String

--- 8_Recursion2/Recursion2.java
+++ 8_Recursion2/Recursion2.java
@@ -80,3 +80,34 @@
 //         System.out.println(friendsPairing(3));
 //     }
 // }
+
+
+// Print Binary String
+// Given a number n, we need to print all binary strings of size n such that there are no consecutive 1’s in the string.
+// public class Recursion2 {
+//     public static void printBinaryString(int n , int lastPlace, String str){
+//         //base case
+//         if(n == 0){
+//             System.out.println(str);
+//             return;
+//         }
+//         //kaam
+//         // if(lastPlace == 0){
+//         //     //sit 0 on chair n
+//         //     printBinaryString(n-1, 0, str.append("0"));
+//         //     printBinaryString(n-1, 1, str.append("1"));
+//         // }else{
+//         //     //sit 1 on chair n
+//         //     printBinaryString(n-1, 0, str.append("0"));
+//         // }
+
+//         printBinaryString(n-1, 0, str+"0");
+//         if(lastPlace == 0){
+//             printBinaryString(n-1, 1, str+"1");
+//         }    
+//     }
+
+//     public static void main(String[] args) {
+//         printBinaryString(3, 0, "");        
+//     }
+// }
