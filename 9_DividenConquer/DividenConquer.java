ba231c11d44031fe32eb13b2bd796748d0611bc1
Pratik Salunke <pratiksalunke3112@gmail.com>
2026-03-30 16:11:58 +0530

    // Quick Sort

--- 9_DividenConquer/DividenConquer.java
+++ 9_DividenConquer/DividenConquer.java
@@ -1,4 +1,5 @@
 // Divide and Conquer
+// Merge Sort
 // public class DividenConquer {
 //     public static void  printArr(int arr[]){
 //         for(int i = 0; i < arr.length; i++){
@@ -64,6 +65,57 @@
 //     public static void main(String[] args) {
 //         int arr[] = {6, 3, 9, 5, 2, 8};
 //         mergeSort(arr, 0, arr.length-1);
+//         printArr(arr);
+//     }
+// }
+
+
+// Quick Sort
+// public class DividenConquer {
+//     public static void  printArr(int arr[]){
+//         for(int i = 0; i < arr.length; i++){
+//             System.out.print(arr[i] + " ");
+//         }
+//         System.out.println();
+//     }
+
+//     public static void quickSort(int arr[], int si, int ei){
+//         //base case
+//         if(si >= ei){
+//             return;
+//             }
+//         // last element as pivot
+//         int pIdx = partition(arr, si, ei);
+//         //left part
+//         quickSort(arr, si, pIdx-1);
+//         //right part
+//         quickSort(arr, pIdx+1, ei);
+//     }
+
+//     public static int partition(int arr[] , int si , int ei) {
+//         int pivot = arr[ei];
+//         int i = si - 1; // to track the smaller element index
+
+//         for(int j = si; j < ei; j++){
+//             if(arr[j] <= pivot){
+//                 i++;
+//                 //swap
+//                 int temp = arr[j];
+//                 arr[j] = arr[i];
+//                 arr[i] = temp;
+//             }
+//         }
+//         //swap pivot to correct position
+//         i++;
+//         int temp = pivot;
+//         arr[ei] = arr[i];
+//         arr[i] = temp;
+//         return i; // return pivot index
+//     }
+
+//     public static void main(String[] args) {
+//         int arr[] = {6, 3, 9, 5, 2, 8};
+//         quickSort(arr, 0, arr.length-1);
 //         printArr(arr);
 //     }
 // }
