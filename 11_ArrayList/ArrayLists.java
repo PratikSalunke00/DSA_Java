ea4ac829d35d3c6cb0a19e18d55bb9e84ee07bda
Pratik Salunke <pratiksalunke3112@gmail.com>
2026-06-14 08:36:07 +0530

    //to print max of the arraylist  O(n)

--- 11_ArrayList/ArrayLists.java
+++ 11_ArrayList/ArrayLists.java
@@ -12,37 +12,53 @@ public class ArrayLists{
         list.add(3);
         list.add(4);
         list.add(5);
-        list.add(1, 9); //O(n)
-        System.out.println(list);
+        list.add(8);
+        // list.add(1, 9); //O(n)
+        // System.out.println(list);
 
-        //Get operation  //O(1)
-        int element = list.get(2);
-        System.out.println(element);
+        // //Get operation  //O(1)
+        // int element = list.get(2);
+        // System.out.println(element);
 
-        //Remove operation  //O(n)
-        int removeEl = list.remove(2);
-        System.out.println(list); 
+        // //Remove operation  //O(n)
+        // int removeEl = list.remove(2);
+        // System.out.println(list); 
 
-        //Set element at index  //O(n)
-        list.set(2, 10);
-        System.out.println(list); 
+        // //Set element at index  //O(n)
+        // list.set(2, 10);
+        // System.out.println(list); 
 
-        //Contains element  //O(n)
-        System.out.println(list.contains(1)); 
-        System.out.println(list.contains(11)); 
+        // //Contains element  //O(n)
+        // System.out.println(list.contains(1)); 
+        // System.out.println(list.contains(11)); 
 
-        //Size of ArrayList
-        System.out.println(list.size()); 
+        // //Size of ArrayList
+        // System.out.println(list.size()); 
 
-        //print the arraylist
+        // //print the arraylist
+        // for(int i=0; i<list.size(); i++){
+        //     System.out.print(list.get(i) + " ");
+        // }
+
+        // //Reverse the ArrayList  O(n)
+        // for(int i=list.size()-1; i>=0; i--){
+        //     System.out.print(list.get(i) + " ");
+        // }
+
+
+        //to print max of the arraylist  O(n)
+        int max = Integer.MIN_VALUE;
         for(int i=0; i<list.size(); i++){
-            System.out.print(list.get(i) + " ");
+            // if(list.get(i) > max){
+            //     max = list.get(i);
+            // } Or
+
+            max = Math.max(max, list.get(i));
         }
 
-        //Reverse the ArrayList  O(n)
-        for(int i=list.size()-1; i>=0; i--){
-            System.out.print(list.get(i) + " ");
-        }
+        System.out.println("Max element:" + max);
+
+
     }
 }
 
