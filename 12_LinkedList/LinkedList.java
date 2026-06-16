b84f68f723f52de92f575e88f6de7bbced298e5a
Pratik Salunke <pratiksalunke3112@gmail.com>
2026-06-16 09:18:12 +0530

    remove method

--- 12_LinkedList/LinkedList.java
+++ 12_LinkedList/LinkedList.java
@@ -93,6 +93,30 @@ public class LinkedList{
         return val;
     }
 
+    //remove last
+    public int removeLast(){
+        if(size == 0){
+            System.out.println("LinkedList is empty");
+            return Integer.MIN_VALUE;
+        }
+        else if(size == 1){
+            int val = head.data;
+            head = tail = null;
+            size = 0;
+            return val;
+        }
+        //prev : i = size-2
+        Node prev = head;
+        for(int i=0; i<size-2; i++){
+            prev = prev.next;
+        }
+        int val = prev.next.data; //tail.data
+        prev.next = null;
+        tail = prev;
+        size--;
+        return val;
+    }
+
     public static void main(String args[]) {
         LinkedList ll = new LinkedList();
         ll.addFirst(2);
@@ -106,6 +130,10 @@ public class LinkedList{
         System.out.println(ll.removeFirst()); // 1
         System.out.println(size); // 4
         ll.print(); // 2->9->3->4->null
+
+        ll.removeLast();
+        ll.print(); // 2->9->3->null
+        System.out.println(size); // 3
     }
 }
 
