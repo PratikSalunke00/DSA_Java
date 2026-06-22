ae5ae6000439a25b327b37043c8356421ac5b7a9
Pratik Salunke <pratiksalunke3112@gmail.com>
2026-06-22 12:14:09 +0530

    //Question 8 : Maximum Area Histogram

--- 13_Stack/Stack.java
+++ 13_Stack/Stack.java
@@ -346,36 +346,96 @@
 
 
 //Question 7 : Duplicate Parentheses
+// import java.util.*;
+// public class Stack{
+//     public static boolean isDuplicate(String str) {
+//         java.util.Stack<Character> s = new java.util.Stack<>();
+
+//         for(int i=0; i<str.length(); i++) {
+//             char ch = str.charAt(i);
+
+//             if(ch == ')') {
+//                 int count = 0;
+//                 while(!s.isEmpty() && s.peek() != '(') {
+//                     s.pop();
+//                     count++;
+//                 }
+//                 if(count < 1) {
+//                     return true;
+//                 } else {
+//                     s.pop(); //pop opening bracket
+//                 }
+//             } else {
+//                 s.push(ch);
+//             }
+//         }
+//         return false;
+//     }
+
+//     public static void main(String args[]) {
+//         String str = "((a+b))"; //true
+//         String str1 = "(a+b)"; //false
+//         System.out.println(isDuplicate(str));
+//         System.out.println(isDuplicate(str1));
+//     }
+// }
+
+
+//Question 8 : Maximum Area Histogram
 import java.util.*;
 public class Stack{
-    public static boolean isDuplicate(String str) {
-        java.util.Stack<Character> s = new java.util.Stack<>();
+    public static void maxArea(int arr[]) {
+        int maxArea=0;
+        int nsr[]=new int[arr.length];
+        int nsl[]= new int[arr.length];
 
-        for(int i=0; i<str.length(); i++) {
-            char ch = str.charAt(i);
+        //Next smaller right
+        java.util.Stack<Integer> s = new java.util.Stack<>();
 
-            if(ch == ')') {
-                int count = 0;
-                while(!s.isEmpty() && s.peek() != '(') {
-                    s.pop();
-                    count++;
-                }
-                if(count < 1) {
-                    return true;
-                } else {
-                    s.pop(); //pop opening bracket
-                }
-            } else {
-                s.push(ch);
+        for(int i=arr.length-1; i>=0; i--) {
+            while(!s.isEmpty() && arr[s.peek()] >= arr[i]) {
+                s.pop();
             }
+            if(s.isEmpty()) {
+                //-1
+                nsr[i] = arr.length;
+            }else{
+                //top
+                nsr[i] = s.peek();
+            }
+            s.push(i);
         }
-        return false;
-    }
 
+        //next smaller left
+        s = new java.util.Stack<>();
+
+        for(int i=0; i<arr.length; i++) {
+            while(!s.isEmpty() && arr[s.peek()] >= arr[i]) {
+                s.pop();
+            }
+            if(s.isEmpty()) {
+                //-1
+                nsl[i] = -1;
+            }else{
+                //top
+                nsl[i] = s.peek();
+            }
+            s.push(i);
+        }
+
+        //calculate current area  : width = j-i-1 = nsr[i] - nsl[i]-1
+        for(int i=0; i<arr.length; i++) {
+            int height = arr[i];
+            int width  = nsr[i] - nsl[i] - 1;
+            int currArea = height * width;
+            maxArea = Math.max(currArea, maxArea);
+        }
+
+        System.out.println("max area in histogram=" + maxArea);
+
+    }
     public static void main(String args[]) {
-        String str = "((a+b))"; //true
-        String str1 = "(a+b)"; //false
-        System.out.println(isDuplicate(str));
-        System.out.println(isDuplicate(str1));
+        int arr[] = {2,1,5,6,2,3}; //heights of histogram bars
+        maxArea(arr);
     }
 }
