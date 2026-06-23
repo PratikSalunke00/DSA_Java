5a0c009d585d25a93bc19be9ba665928aa933bdc
Pratik Salunke <pratiksalunke3112@gmail.com>
2026-06-23 13:06:49 +0530

    //Queue using Linked List

--- 14_Queue/Queue.java
+++ 14_Queue/Queue.java
@@ -71,44 +71,122 @@
 
 
 //Circular queue using array
+// import java.util.*;
+// public class Queue{
+//     static class QueueB{
+//         static int arr[];
+//         static int size;
+//         static int rear;
+//         static int front;
+
+//         QueueB(int n) {
+//             arr = new int[n];
+//             size = n;
+//             rear = -1;
+//             front = -1;
+//         }
+
+//         public static boolean isEmpty() {
+//             return rear == -1 && front == -1;
+//         }
+
+//         //check queue is full 
+//         public static boolean isFull() {
+//             return (rear+1) % size == front;
+//         }
+
+//         //add
+//         public static void add(int data) {
+//             if(isFull()){
+//                 System.out.println("queue is full");
+//                 return;
+//             }
+
+//             //add 1st element
+//             if(front == -1) {
+//                 front = 0;
+//             }
+
+//             rear = (rear + 1 ) % size;
+//             arr[rear] =data;
+//         }
+
+//         //remove 
+//         public static int remove() {
+//             if(isEmpty()) {
+//                 System.out.println("empty queue");
+//                 return -1;
+//             }
+
+//             int result = arr[front];
+//             //when last el delete
+//             if(rear == front) {
+//                 rear = front = -1;
+//             }else{
+//                 front =(front + 1) % size;
+//             }
+//             return result;
+//         }
+
+//         //peek
+//         public static int peek() {
+//             if(isEmpty()) {
+//                 System.out.println("empty queue");
+//                 return -1;
+//             }
+
+//             return arr[front];
+//         }
+//     }
+
+//     public static void main(String args[]) {
+//         QueueB q = new QueueB(3);
+//         q.add(1);
+//         q.add(2);
+//         q.add(3);
+//         System.out.println(q.remove());
+//         q.add(4);
+//         System.out.println(q.remove());
+//         q.add(5);
+
+//         while(!q.isEmpty()) {
+//             System.out.println(q.peek());
+//             q.remove();
+//         }
+//     }
+// }
+
+
+//Queue using Linked List
 import java.util.*;
 public class Queue{
-    static class QueueB{
-        static int arr[];
-        static int size;
-        static int rear;
-        static int front;
+    static class Node{
+        int data;
+        Node next;
 
-        QueueB(int n) {
-            arr = new int[n];
-            size = n;
-            rear = -1;
-            front = -1;
+        Node(int data) {
+            this.data = data;
+            this.next = null;
         }
+    }
+
+    static class QueueB{
+        static Node head = null;
+        static Node tail = null;
 
         public static boolean isEmpty() {
-            return rear == -1 && front == -1;
-        }
-
-        //check queue is full 
-        public static boolean isFull() {
-            return (rear+1) % size == front;
+            return head == null && tail == null;
         }
 
         //add
         public static void add(int data) {
-            if(isFull()){
-                System.out.println("queue is full");
+            Node newNode =  new Node(data);
+            if(head == null) {
+                head = tail = newNode;
                 return;
             }
-
-            //add 1st element
-            if(front == -1) {
-                front = 0;
-            }
-
-            rear = (rear + 1 ) % size;
-            arr[rear] =data;
+            tail.next = newNode;
+            tail = newNode; //changed tail
         }
 
         //remove 
@@ -118,30 +196,29 @@ public class Queue{
                 return -1;
             }
 
-
-            int result = arr[front];
-            //when last el delete
-            if(rear == front) {
-                rear = front = -1;
+            int front = head.data;
+            //single el
+            if(tail == head) {
+                tail = head = null;
             }else{
-                front =(front + 1) % size;
+                head = head.next;
             }
-            return result;
+            return front;
         }
 
-        //peek
+         //peek
         public static int peek() {
             if(isEmpty()) {
                 System.out.println("empty queue");
                 return -1;
             }
 
-            return arr[front];
+            return head.data;
         }
     }
 
     public static void main(String args[]) {
-        QueueB q = new QueueB(3);
+        QueueB q = new QueueB();
         q.add(1);
         q.add(2);
         q.add(3);
