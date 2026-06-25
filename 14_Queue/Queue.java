825c6fd9cfa6d29abe951c01706774f172292c15
Pratik Salunke <pratiksalunke3112@gmail.com>
2026-06-25 12:50:30 +0530

    Stack and Queue using Deque

--- 14_Queue/Queue.java
+++ 14_Queue/Queue.java
@@ -511,25 +511,90 @@
 
 
 //Deque
+// import java.util.*;
+// public class Queue{
+//     public static void main(String args[]) {
+//         Deque<Integer> deque = new LinkedList<>();
+//         deque.addFirst(1);
+//         deque.addFirst(2);
+//         System.out.println(deque);//2 1
+
+//         deque.removeFirst();
+//         System.out.println(deque);//1
+
+//         deque.addLast(3);
+//         deque.addLast(4);
+//         System.out.println(deque);
+
+//         deque.removeLast();
+//         System.out.println(deque);
+
+//         System.out.println("first el = " + deque.getFirst());
+//         System.out.println("last el = " + deque.getLast());
+//     }
+// }
+
+
+//Stack using Deque
+// import java.util.*;
+// public class Queue{
+//     static class Stack{
+//         Deque<Integer> deque = new LinkedList<>();
+
+//         public void push(int data) {
+//             deque.addLast(data);
+//         }
+
+//         public int pop() {
+//             return deque.removeLast();
+//         }
+
+//         public int peek() {
+//             return deque.getLast();
+//         }
+//     }
+//     public static void main(String args[]) {
+//         Stack s = new Stack();
+//         s.push(1);
+//         s.push(2);
+//         s.push(3);
+       
+//         System.out.println("peek = " + s.peek());
+//         System.out.println(s.pop());
+//         System.out.println(s.pop());
+//         System.out.println(s.pop());
+//     }
+// }
+
+
+//Queue using Deque
 import java.util.*;
 public class Queue{
-    public static void main(String args[]) {
+    static class QueueB{
         Deque<Integer> deque = new LinkedList<>();
-        deque.addFirst(1);
-        deque.addFirst(2);
-        System.out.println(deque);//2 1
 
-        deque.removeFirst();
-        System.out.println(deque);//1
+        public void add(int data) {
+            deque.addLast(data);
+        }
 
-        deque.addLast(3);
-        deque.addLast(4);
-        System.out.println(deque);
+        public int remove() {
+            return deque.removeFirst();
+        }
 
-        deque.removeLast();
-        System.out.println(deque);
+        public int peek() {
+            return deque.getFirst();
+        }
+    }
 
-        System.out.println("first el = " + deque.getFirst());
-        System.out.println("last el = " + deque.getLast());
+    public static void main(String args[]) {
+        QueueB q = new QueueB();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+
+        System.out.println("peek =" + q.peek());
+        System.out.println(q.remove());
+        System.out.println(q.remove());
+        System.out.println(q.remove());
     }
 }
