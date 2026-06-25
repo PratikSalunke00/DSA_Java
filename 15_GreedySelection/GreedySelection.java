bf508b1506b52559aadd734b5dc77896d489f063
Pratik Salunke <pratiksalunke3112@gmail.com>
2026-06-25 16:39:04 +0530

    //Greedy Selection //Activity selection

---
+++ 15_GreedySelection/GreedySelection.java
@@ -0,0 +1,59 @@
+//Greedy Selection
+//Activity selection
+import java.util.*;
+public class GreedySelection{
+    public static void main(String args[]) {
+        int start[] = {1,3,0,5,8,5};
+        int end[] = {2,4,6,7,9,9}; //sorted acivity
+
+        //for sorting process
+        int activities[] [] = new int[start.length] [3];
+        for(int i=0; i<start.length; i++) {
+            activities[i] [0] =i;
+            activities[i] [1] =start[i];
+            activities[i] [2] =end[i];
+        }
+
+        //lambda function-> shortform of big function
+        Arrays.sort(activities, Comparator.comparingDouble(o -> o[2]));
+        //end time basis sorted
+        int maxAct = 0;
+        ArrayList<Integer> ans = new ArrayList<>();
+
+        //1st activity
+        // maxAct = 1;
+        // ans.add(0);
+        // ans.add(activities [0][0]);
+        // int lastEnd = end[0];
+        // int lastEnd = activities[0][2];
+
+        // for(int i=1; i<end.length; i++) {
+        //     if(start[i] >= lastEnd) {
+        //         //activity select
+        //         maxAct++;
+        //         ans.add(i);
+        //         lastEnd = end[i];
+        //     }
+        // }
+
+        //for sorting process
+        maxAct = 1;
+        ans.add(activities [0][0]);
+        int lastEnd = activities[0][2];
+
+        for(int i=1; i<end.length; i++) {
+            if(activities[i][1] >= lastEnd) {
+                //activity select
+                maxAct++;
+                ans.add(activities[i][0]);
+                lastEnd = activities[i][2];
+            }
+        }
+
+        System.out.println("max activities = " + maxAct);
+        for(int i=0; i<ans.size(); i++) {
+            System.out.print("A" + ans.get(i)+ " ");
+        }
+        System.out.println();
+    }
+}
