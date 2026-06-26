c5cf82f2af9cc2b315eebdb02821090ce6f6b0e5
Pratik Salunke <pratiksalunke3112@gmail.com>
2026-06-26 21:42:15 +0530

    //Indian Coins

--- 15_GreedySelection/GreedySelection.java
+++ 15_GreedySelection/GreedySelection.java
@@ -122,21 +122,51 @@
 
 
 //Max length chain of pairs
+// import java.util.*;
+// public class GreedySelection{
+//     public static void main(String args[]) {
+//         int pairs[][] = {{5,24} , {39,60}, {5,28},{27,40},{50,90}};
+//         Arrays.sort(pairs, Comparator.comparingDouble(o -> o[1]));
+
+//         int chainLen = 1;
+//         int chainEnd = pairs[0][1]; //last selected pair end
+        
+//         for(int i=1; i<pairs.length; i++){
+//             if(pairs[i][0] > chainEnd) {
+//                 chainLen++;
+//                 chainEnd = pairs[i][1];
+//             }
+//         }
+//         System.out.println("max length of chain = " + chainLen);
+//     }
+// }
+
+
+//Indian Coins
 import java.util.*;
 public class GreedySelection{
     public static void main(String args[]) {
-        int pairs[][] = {{5,24} , {39,60}, {5,28},{27,40},{50,90}};
-        Arrays.sort(pairs, Comparator.comparingDouble(o -> o[1]));
+        Integer coins[] = {1,2,5,10,20,50,100,500,2000};
+        Arrays.sort(coins, Comparator.reverseOrder());
 
-        int chainLen = 1;
-        int chainEnd = pairs[0][1]; //last selected pair end
+        int countOfCoins = 0;
+        int amount = 590; //last selected pair end
+        ArrayList<Integer> ans = new ArrayList<>();
         
-        for(int i=1; i<pairs.length; i++){
-            if(pairs[i][0] > chainEnd) {
-                chainLen++;
-                chainEnd = pairs[i][1];
+        for(int i=0; i<coins.length; i++){
+            if(coins[i] <= amount) {
+                while(coins[i] <= amount ) {
+                    countOfCoins++;
+                    ans.add(coins[i]);
+                    amount -= coins[i];
+                }
             }
         }
-        System.out.println("max length of chain = " + chainLen);
+        System.out.println("total min coins used = " + countOfCoins);
+
+        for(int i = 0; i<ans.size(); i++) {
+            System.out.print(ans.get(i) + " ");
+        }
+        System.out.println();
     }
 }
