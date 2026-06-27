93f47e5f53e6b407fccbae7cc2824fd13697d0c4
Pratik Salunke <pratiksalunke3112@gmail.com>
2026-06-27 20:19:34 +0530

    //Chocola Problem

--- 15_GreedySelection/GreedySelection.java
+++ 15_GreedySelection/GreedySelection.java
@@ -173,44 +173,87 @@
 
 
 //Job sequencing Problem
+// import java.util.*;
+// public class GreedySelection{
+//     static class Job{
+//         int deadline;
+//         int profit;
+//         int id;
+
+//         public Job(int i, int d, int p) {
+//             id=i;
+//             deadline=d;
+//             profit=p;
+//         }
+//     }
+//     public static void main(String args[]) {
+//         int jobInfo[][] = {{4,20},{1,10},{1,40},{1,30}};
+
+//         ArrayList<Job> jobs = new ArrayList<>();
+
+//         for(int i= 0; i<jobInfo.length; i++) {
+//             jobs.add(new Job(i, jobInfo[i][0], jobInfo[i][1]));
+//         }
+
+//         Collections.sort(jobs, (obj1,obj2) -> obj2.profit-obj1.profit);  //descending order sort
+
+//         ArrayList<Integer> seq = new ArrayList<>();
+//         int time =0;
+//         for(int i=0; i<jobs.size(); i++) {
+//             Job curr = jobs.get(i);
+//             if(curr.deadline>time){
+//                 seq.add(curr.id);
+//                 time++;
+//             }
+//         }
+
+//         System.out.println("max jobs =" + seq.size());
+//         for(int i=0; i<seq.size(); i++) {
+//             System.out.print( seq.get(i)+ " ");
+//         }
+//         System.out.println();
+//     }
+// }
+
+
+//Chocola Problem
 import java.util.*;
 public class GreedySelection{
-    static class Job{
-        int deadline;
-        int profit;
-        int id;
-
-        public Job(int i, int d, int p) {
-            id=i;
-            deadline=d;
-            profit=p;
-        }
-    }
     public static void main(String args[]) {
-        int jobInfo[][] = {{4,20},{1,10},{1,40},{1,30}};
+        int n =4 , m =6;
+        Integer costVer[] = {2,1,3,1,4}; //m-1
+        Integer costHor[] = {4,1,2}; //n-1
 
-        ArrayList<Job> jobs = new ArrayList<>();
+        Arrays.sort(costVer, Collections.reverseOrder());
+        Arrays.sort(costHor, Collections.reverseOrder());
+        
+        int hc=0, vc=0;
+        int hp=1, vp=1; //because we have single piece at starting
+        int cost = 0;
 
-        for(int i= 0; i<jobInfo.length; i++) {
-            jobs.add(new Job(i, jobInfo[i][0], jobInfo[i][1]));
-        }
-
-        Collections.sort(jobs, (obj1,obj2) -> obj2.profit-obj1.profit);  //descending order sort
-
-        ArrayList<Integer> seq = new ArrayList<>();
-        int time =0;
-        for(int i=0; i<jobs.size(); i++) {
-            Job curr = jobs.get(i);
-            if(curr.deadline>time){
-                seq.add(curr.id);
-                time++;
+        while(hc < costHor.length && vc < costVer.length) {
+            if(costVer[vc] <= costHor[hc]) { //Horizontal cut
+                cost += (costHor[hc] * vp);
+                hp++;
+                hc++;
+            } else {
+                cost += (costVer[vc] * hp);
+                vp++;
+                vc++;
             }
         }
 
-        System.out.println("max jobs =" + seq.size());
-        for(int i=0; i<seq.size(); i++) {
-            System.out.print( seq.get(i)+ " ");
+        while(hc < costHor.length) {
+            cost += (costHor[hc] * vp);
+            hp++;
+            hc++;
         }
-        System.out.println();
+
+        while(vc < costVer.length) {
+            cost += (costVer[vc] * hp);
+            vp++;
+            vc++;
+        }
+        System.out.println("min cost of cuts = " + cost);
     }
 }
